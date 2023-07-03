4332d41c2ba7dac57b0bdca78c6c9cb13fed20c4
Shalini Maurya <90104709+shalinimaurya17@users.noreply.github.com>
2023-07-03 12:49:23 +0530

    Updates

---
+++ precache-manifest.e8a9ca49aec5715b988dbc1dbf5cbae5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "66022ff4f0e48bc95a02",
+    "url": "/NewzMeow/static/css/main.17fbe5ae.chunk.css"
+  },
+  {
+    "revision": "66022ff4f0e48bc95a02",
+    "url": "/NewzMeow/static/js/main.fc7f2e75.chunk.js"
+  },
+  {
+    "revision": "7f48dc614a8a5d8c3ef2",
+    "url": "/NewzMeow/static/js/runtime~main.3fe988a9.js"
+  },
+  {
+    "revision": "ec0e4d36a4829e0e6653",
+    "url": "/NewzMeow/static/js/2.7ac07110.chunk.js"
+  },
+  {
+    "revision": "03c2247ebef71b42a125",
+    "url": "/NewzMeow/static/js/3.133d7e0c.chunk.js"
+  },
+  {
+    "revision": "f1ff94bac1fb084e60dbdd7bc24c4f3c",
+    "url": "/NewzMeow/static/media/loading.gif.f1ff94ba.gif"
+  },
+  {
+    "revision": "c406a8f387378142d54cc4bd368e91bb",
+    "url": "/NewzMeow/index.html"
+  }
+];
